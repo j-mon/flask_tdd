5f69217f15c0c5045784bc5db139ac0e4d50f40b
Jeff Monsalve <monsalve.jeffrey@gmail.com>
2019-02-09 20:42:52 -0500

    created first, then past first test, .gitignore

--- app-test.py
+++ app-test.py
@@ -3,8 +3,9 @@ from app import app
 import unittest
 
 
-class BasicTestClass(unittest.TestCase):
-    def text_index(self):
+class BasicTestCase(unittest.TestCase):
+
+    def test_index(self):
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
@@ -13,5 +14,3 @@ class BasicTestClass(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
-
