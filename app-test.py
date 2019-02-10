c5003ab7b1633a58b9d359973441876e1113153a
Jeff Monsalve <monsalve.jeffrey@gmail.com>
2019-02-09 20:34:30 -0500

    ValueError: SyntaxError fix

--- app-test.py
+++ app-test.py
@@ -6,7 +6,7 @@ import unittest
 class BasicTestClass(unittest.TestCase):
     def text_index(self):
         tester = app.test_client(self)
-        response = tester.get('/'. content_type='html/text')
+        response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, b'Hello, World!')
 
