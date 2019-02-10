2d09d96f8b31020888f404725aa54c9a6a7ff12c
Jeff Monsalve <monsalve.jeffrey@gmail.com>
2019-02-09 20:32:32 -0500

    updat defined function

--- app-test.py
+++ app-test.py
@@ -4,7 +4,7 @@ import unittest
 
 
 class BasicTestClass(unittest.TestCase):
-# Testing whether response that we get is "200" and "Hello, World!" is displayed    def text_index(self):
+    def text_index(self):
         tester = app.test_client(self)
         response = tester.get('/'. content_type='html/text')
         self.assertEqual(response.status_code, 200)
