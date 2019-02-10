aaba83a4c7ea1aa6f7f3f6f4e5a90c7cda0e28a7
Jeff Monsalve <monsalve.jeffrey@gmail.com>
2019-02-09 21:07:36 -0500

    second_test

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+#imports
+import sqlite3 
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash, jsonify
 
+
+# configuration
+DATABASE = 'flaskr.db'
+DEBUG = True
+SECRET_KEY = 'my_precious'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+
+# create and initialize app
 app = Flask(__name__)
-
-#flask routes to your browser to excecute view function
-@app.route('/')
-def hello():
-    return 'Hello, World!'
-
+app.config.from_object(__name__)
 
 
 if __name__ == '__main__':
